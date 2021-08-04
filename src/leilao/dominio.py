7c6ea4f5e2f5f9c9b2229366b4a45ffe7823f7d3
VictorGM01 <victormarques8801@gmail.com>
2021-08-03 22:51:52 -0300

    # arquivo de teste

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -23,9 +23,12 @@ class Leilao:
         self.descricao = descricao
         self.__lances = []
 
+    def propoe_lance(self, lance: Lance):
+        self.__lances.append(lance)
+
     @property
     def lances(self):
-        return self.__lances
+        return self.__lances[:]
 
 
 class Avaliador:
