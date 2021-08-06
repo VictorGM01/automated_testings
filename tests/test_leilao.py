713bb49200d937896b63f786671e732ec6fa48dc
VictorGM01 <victormarques8801@gmail.com>
2021-08-05 22:19:49 -0300

    # testa as aplicações do leilão

--- tests/test_leilao.py
+++ tests/test_leilao.py
@@ -5,13 +5,13 @@ from src.leilao.dominio import Usuario, Lance, Leilao
 class TestLeilao(TestCase):
 
     def setUp(self):
-        self.rapha = Usuario("Raphaela")
+        self.rapha = Usuario("Raphaela", 1500.0)
         self.lance_da_rapha = Lance(self.rapha, 950.0)
         self.leilao = Leilao('Quadro: Monalisa')
         self.menor_valor_esperado = 950.0
 
     def test_deve_retornar_o_maior_e_o_menor_numero_de_dois_lances_quando_adicionados_em_ordem_crescente(self):
-        victor = Usuario('Victor')
+        victor = Usuario('Victor', 1500.0)
 
         lance_do_victor = Lance(victor, 1000.0)
 
@@ -25,7 +25,7 @@ class TestLeilao(TestCase):
 
     def test_nao_deve_permitir_propor_o_lance_caso_o_valor_seja_menor_do_que_o_valor_do_ultimo_lance(self):
         with self.assertRaises(ValueError):
-            victor = Usuario('Victor')
+            victor = Usuario('Victor', 1500.0)
 
             lance_do_victor = Lance(victor, 1000.0)
 
@@ -41,8 +41,8 @@ class TestLeilao(TestCase):
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_lance_quando_existir_mais_do_que_dois_lances(self):
-        victor = Usuario('Victor')
-        najara = Usuario("Najara")
+        victor = Usuario('Victor', 1500.0)
+        najara = Usuario("Najara", 1500.0)
 
         lance_do_victor = Lance(victor, 959.9)
         lance_da_najara = Lance(najara, 1000.0)
@@ -66,7 +66,7 @@ class TestLeilao(TestCase):
     # verifica se o último usuário é diferente do usuário que está propondo o lance, caso sim, o usuário consegue propor:
 
     def test_deve_permitir_o_usuario_propor_o_lance_caso_o_ultimo_usuario_seja_diferente(self):
-        victor = Usuario("Victor")
+        victor = Usuario("Victor", 1500.0)
         lance_do_victor = Lance(victor, 1500.0)
 
         self.leilao.propoe_lance(self.lance_da_rapha)
